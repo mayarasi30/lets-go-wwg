f1567b858a07ae9b83f4572ba8e494580443c047
mayarasi30 <87201686+mayarasi30@users.noreply.github.com>
2021-07-23 16:41:36 -0300

    Exercício #01
    Existem dois times de futebol, o time amarelo e o time vermelho. O time amarelo tem 5 jogadores (Fernando, João, Lúcia, Mariana e Ana) e o time vermelho tem 4 jogadores (Helena, Jonas, José e Juliana).
    Crie um array de string para cada time e nomeie com o nome do time. 
    Printe na tela os nomes dos jogadores de cada time

---
+++ exExtra/TipoComposto/ex1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var TimeAmarelo = [5]string{"Fernando", "João", "Lúcia", "Mariana", "Ana"}
+	var TimeVermelho = [4]string{"Helena", "Jonas", "José", "Juliana"}
+	
+	fmt.Println("Time Amarelo: ",TimeAmarelo)
+	fmt.Println("Time Vermelho: ",TimeVermelho)
+}
